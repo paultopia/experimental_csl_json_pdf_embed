d930c8e9dd407e8a1183ac20674ab4cdad5a9888
Paul Gowder <paul.gowder@gmail.com>
2024-02-25 10:32:48 -0600

    adding metadata appears to work

--- coherent/pgtestworker.js
+++ coherent/pgtestworker.js
@@ -6,21 +6,25 @@ self.onmessage = function(e) {
      case 'pdf':
        //Load the PDF from the array of bytes handed to us by index.html
        var pdf = coherentpdf.fromMemory(e.data.bytes, "");
+       var metadata = e.data.metadata;
        coherentpdf.setFast();
-       self.postMessage({mtype: 'progress', message: '(1/4) PDF loaded successfully from file ...'});
+       self.postMessage({mtype: 'progress', message: '(1/5) PDF loaded successfully from file ...'});
        //Send some metadata back to index.html
        self.postMessage({mtype: 'pages', x: coherentpdf.pages(pdf)});
        self.postMessage({mtype: 'creator', x: coherentpdf.getCreator(pdf)});
        self.postMessage({mtype: 'producer', x: coherentpdf.getProducer(pdf)});
        //If the PDF is encrypted with blank owner password, decrypt it.
        coherentpdf.decryptPdf(pdf, "");
-       self.postMessage({mtype: 'progress', message: '(2/4) File decrypted if necessary...'});
+       self.postMessage({mtype: 'progress', message: '(2/5) File decrypted if necessary...'});
        //Rotate the contents of each page by 10 degrees
        coherentpdf.rotateContents(pdf, coherentpdf.all(pdf), 10);
-       self.postMessage({mtype: 'progress', message: '(3/4) File rotated....'});
+       self.postMessage({mtype: 'progress', message: '(3/5) File rotated....'});
+       //Add metadata
+       coherentpdf.setMetadataFromByteArray(pdf, metadata);
+       self.postMessage({mtype: 'progress', message: '(4/5) Metadata Added....'});
        //Write the result to a PDF file as an array of bytes
        var mem = coherentpdf.toMemory(pdf, false, false);
-       self.postMessage({mtype: 'progress', message: '(4/4) File serialized to memory...'});
+       self.postMessage({mtype: 'progress', message: '(5/5) File serialized to memory...'});
        //Send the file back to index.html
        self.postMessage({mtype: 'pdfout', bytes: mem});
        //This worker will be terminated by index.html, so no need to call coherentpdf.deletePdf
--- coherent/readme.md
+++ coherent/readme.md
@@ -1,1 +1,38 @@
 working off the example code in https://github.com/coherentgraphics/coherentpdf.js as a starter to hook up to a web page
+
+# NOTES
+
+The strategy:
+
+Turn the JSON file to base64
+Shove the base64 string into a single property basically 
+
+--
+CURRENTLY
+- I can read xml file as xmp metadata and shove it into file, and then inspecting the file with cat shows it in there (though I need to pull out the metadata to make sure it's readable by the library)
+
+---
+
+NEXT STEP: 
+
+Add some function to pull out the metadata and send it back to the original file.  Actually that should be pretty easy...
+
+Then swap out json file for the dummy metadata file: 
+
+Take json file
+Read as text
+Convert to base64 string
+Put into xml in right spot using some kind of string template
+Then encode to uint8 https://stackoverflow.com/questions/6965107/converting-between-strings-and-arraybuffers
+Then shove into file with existing code.
+
+
+----
+
+second try for PDF metadata from browser:
+
+- https://stackoverflow.com/questions/73165537/how-to-add-custom-metadata-to-pdf-using-javascript 
+- https://www.npmjs.com/package/coherentpdf
+- https://www.coherentpdf.com/jscpdf/index.html
+- https://github.com/coherentgraphics/coherentpdf.js/blob/master/index.html
+- modify with json upload code from here: https://github.com/paultopia/experimental_csl_json_pdf_embed/blob/main/index.html 
