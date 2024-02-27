492c387921398e5cbe4a7b53035ad9533bd754b7
Paul Gowder <paul.gowder@gmail.com>
2024-02-26 22:07:10 -0600

    first draft of cleaned code should work but will probably have bugs

---
+++ clean_working/js/pdf_worker.js
@@ -0,0 +1,27 @@
+importScripts('js/coherentpdf.browser.min.js')
+
+self.onmessage = function(e) {
+   switch (e.data.mtype)
+    {
+	case 'embed':
+	  var pdf = coherentpdf.fromMemory(e.data.bytes, "");
+	  var metadata = e.data.metadata;
+	  coherentpdf.setFast();
+	  self.postMessage({mtype: 'progress', message: 'PDF loaded successfully for embedding. Working ...'});
+	  coherentpdf.decryptPdf(pdf, "");
+	  coherentpdf.setMetadataFromByteArray(pdf, metadata);
+	  var mem = coherentpdf.toMemory(pdf, false, false);
+	  self.postMessage({mtype: 'pdfout', bytes: mem});
+	break;
+	case 'extract':
+	  var pdf = coherentpdf.fromMemory(e.data.bytes, "");
+	  coherentpdf.setFast();
+          self.postMessage({mtype: 'progress', message: 'PDF loaded successfully for extraction. Working ...'});
+	  coherentpdf.decryptPdf(pdf, "");
+	  var extractedMetadata = coherentpdf.getMetadata(pdf);
+	  var decoder = new TextDecoder("utf-8");
+	  var decodedMetadata = decoder.decode(extractedMetadata);
+	  self.postMessage({mtype: 'dataExtracted', metadata: decodedMetadata});
+	break;
+    }
+}
